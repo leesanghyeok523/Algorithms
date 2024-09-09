8bdbb48e7bc6c299cc2ebb9c29d9558a1ef6c80c
leesanghyeok523 <130319556+leesanghyeok523@users.noreply.github.com>
2024-09-09 09:01:11 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 1,347 ms, Memory: 55,944 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.py
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.py
@@ -1,20 +1,23 @@
-di, dj = [0,-1,1,0,0], [0,0,0,-1,1]
-tbl = [0, 2, 1, 4, 3]
+# import sys
+# sys.stdin = open('input.txt', 'r')
 
-T = int(input())
-for t in range(1, T + 1) :
+di = [0,-1,1,0,0]
+dj = [0,0,0,-1,1]
+opp = [0,2,1,4,3]
+for t in range(1, int(input()) + 1) :
     N, M, K = map(int,input().split())
     arr = [list(map(int,input().split())) for _ in range(K)]
 
     for _ in range(M) :
+        # [0] : i [1] : j [2] :n [3] :dr
         for i in range(len(arr)) :
             arr[i][0] += di[arr[i][3]]
             arr[i][1] += dj[arr[i][3]]
-            if arr[i][0] == 0 or arr[i][0] == N - 1 or arr[i][1] == 0 or arr[i][1] == N -1 :
+            if arr[i][0] == 0 or arr[i][0] == N - 1 or arr[i][1] == 0 or arr[i][1] == N - 1:
                 arr[i][2] //= 2
-                arr[i][3] = tbl[arr[i][3]]
+                arr[i][3] = opp[arr[i][3]]
 
-        arr.sort(key = lambda x : (x[0],x[1],x[2]), reverse = True)
+        arr.sort(key=lambda x : (x[0],x[1],x[2]), reverse= True)
 
         i = 1
         while i < len(arr) :
@@ -27,4 +30,5 @@ for t in range(1, T + 1) :
     ans = 0
     for lst in arr :
         ans += lst[2]
+
     print(f'#{t} {ans}')
